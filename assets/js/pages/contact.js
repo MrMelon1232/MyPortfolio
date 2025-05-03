4607bdb81b8777d9b8cab7ebfc36ca100287a093
Julien Phan <julien.phan18@gmail.com>
2025-05-02 20:55:29 -0400

    Added contact page and wip css for contact

---
+++ assets/js/pages/contact.js
@@ -0,0 +1,11 @@
+// Fetch our home page to render it
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("pages/contact.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("contact-page").innerHTML = data;
+    })
+
+    // Catch error
+    .catch((error) => console.error("Error loading contact page:", error));
+});
